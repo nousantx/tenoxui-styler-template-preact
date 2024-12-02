eba7a87149a9d05e6c6e8fc0ee9e37c09f1ad511
nousantx <nousantx@gmail.com>
2024-12-02 15:07:20 +0700

    Fix layout

--- src/app.tsx
+++ src/app.tsx
@@ -5,8 +5,15 @@ export function App() {
     <main class="p-2rem h-mm-100vh">
       <article>
         <header>
-          <h1 class="family-poppins text-4xl text-primary-500">Hello World!</h1>
-          <button class="btn bg-neutral-100 hover:bg-neutral-300 text-neutral-900">Click Me</button>
+          <h1 class="family-poppins text-4xl text-neutral-50 fw-500 lh-1 ls--0.040em">
+            Hello World!
+          </h1>
+          <div class="mt-2rem d-flex gap-1rem">
+            <button class="btn bg-neutral-100 hover:bg-neutral-300 text-neutral-900">
+              See Example
+            </button>
+            <button class="btn btn-line hover:bgc-transparent text-neutral-100">Learn More</button>
+          </div>
         </header>
       </article>
     </main>
